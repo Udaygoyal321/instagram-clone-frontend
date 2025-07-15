b09f4f96d7ea21a8dceaeb69b5c0fb19587cb868
udaygoyal567 <udaygoyal567@gmail.com>
2025-07-15 21:46:44 +0530

    Updated API baseURL to live backend URL

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // Backend base URL
+  baseURL: "https://instagram-clone-backend-nd23.onrender.com", // Backend base URL
 });
 
 // attach token to each request
