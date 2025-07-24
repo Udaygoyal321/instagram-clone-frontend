cc2e46118e679b81f1d72e31832fcc64fb0e81fc
udaygoyal567 <udaygoyal567@gmail.com>
2025-07-24 20:41:26 +0530

    connect frontend to backend with env

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, // ✅ Use env variable now
+  baseURL: import.meta.env.REACT_APP_API_URL, // ✅ Use env variable now
   withCredentials: true, // Optional, for cookies/auth
 });
 
